318c1967948086041dac68fd6463b45530165fe0
PranjalRkgp <118005875+PranjalRkgp@users.noreply.github.com>
2025-09-04 10:40:29 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React, { useState } from "react";
+import PipelineToolbar from "./PipelineToolbar";
+import PipelineUI from "./PipelineUI";
+import SubmitButton from "./SubmitButton";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function App() {
+  const [nodes, setNodes] = useState([]);
+  const [edges, setEdges] = useState([]);
+
+  return (
+    <div>
+      <h1>Pipeline Builder</h1>
+      <PipelineToolbar setNodes={setNodes} nodes={nodes} />
+      <PipelineUI nodes={nodes} edges={edges} setEdges={setEdges} />
+      <SubmitButton nodes={nodes} edges={edges} />
+    </div>
+  );
+}
+
+export default App;
